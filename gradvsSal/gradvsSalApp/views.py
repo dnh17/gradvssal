43dba383ac0854abb4c22d95675dfec4892efaed
Damani Basdeo <damanibasdeo@gmail.com>
2017-07-17 07:41:10 -0400

    bootstrap added

--- gradvsSal/gradvsSalApp/views.py
+++ gradvsSal/gradvsSalApp/views.py
@@ -3,9 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404
+from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
+from django.core.exceptions import ObjectDoesNotExist
+
+
+
 from .models import *
 
 def HomeView(request):
     template_name = 'gradvsSalApp/home.html'
+    return render(request, template_name)
